2b0b15c410b51d6a08bf0ec7f7579a29128ac9bd
Guilherme Nascimento <paitomax@gmail.com>
2019-09-14 00:44:46 -0300

    Added Cep Api

---
+++ lib/cep_api.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'cep_api.g.dart';
+
+@RestApi(baseUrl: "http://apps.widenet.com.br/busca-cep/api/")
+abstract class RestClient {
+  factory RestClient(Dio dio) = _RestClient;
+
+  @GET("/cep/{cep}.json")
+  Future<List<CepResponse>> getCep();
+}
+
+@JsonSerializable()
+class CepResponse {
+  int status;
+  String message;
+  String code;
+  String state;
+  String city;
+  String district;
+  String address;
+
+  CepResponse({this.status, this.message, this.code, this.state, this.city, this.district, this.address});
+
+  factory CepResponse.fromJson(Map<String, dynamic> json) => _$CepResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$CepResponseToJson(this);
+}

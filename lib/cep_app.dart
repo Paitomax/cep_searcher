30b16c9b4cf3745caaa2611e3631140afb4ec77e
Guilherme Nascimento <paitomax@gmail.com>
2020-01-04 03:00:06 -0300

    bloc provider created

--- lib/cep_app.dart
+++ lib/cep_app.dart
@@ -1,16 +1,24 @@
+import 'package:cep_searcher/src/home/home_bloc.dart';
 import 'package:cep_searcher/src/home/home_screen.dart';
+import 'package:cep_searcher/src/providers/cep_provider.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CepApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Buscador de endereço',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    var dio = Dio();
+
+    return BlocProvider(
+      create: (context) => HomeBloc(CepProvider(dio)),
+      child: MaterialApp(
+        title: 'Buscador de endereço',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }

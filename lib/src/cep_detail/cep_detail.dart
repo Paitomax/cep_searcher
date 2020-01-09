d3bded78c4a4e48757d5728adb0480801a4f576c
Guilherme Augusto Fonseca Do Nascimento <accenture.gfonseca@bv.com.br>
2020-01-09 17:36:56 -0300

    CepDetailScreen basic info

--- lib/src/cep_detail/cep_detail.dart
+++ lib/src/cep_detail/cep_detail.dart
@@ -1,7 +1,10 @@
+import 'package:cep_searcher/src/models/cep_response.dart';
 import 'package:flutter/material.dart';
 
 class CepDetailScreen extends StatefulWidget {
-  CepDetailScreen({Key key}) : super(key: key);
+  final CepResponse cep;
+
+  CepDetailScreen(this.cep, {Key key}) : super(key: key);
 
   @override
   _CepDetailScreenState createState() {
@@ -11,12 +14,21 @@ class CepDetailScreen extends StatefulWidget {
 
 class _CepDetailScreenState extends State<CepDetailScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.widget.cep.code),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(this.widget.cep.state),
+            Text(this.widget.cep.city),
+            Text(this.widget.cep.district),
+            Text(this.widget.cep.address),
+          ],
+        ),
+      ),
+    );
   }
 }

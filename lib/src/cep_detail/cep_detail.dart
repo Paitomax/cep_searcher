0ce909e54e3b50939221e547a862c7a5e101779b
Guilherme Augusto Fonseca Do Nascimento <accenture.gfonseca@bv.com.br>
2020-01-09 17:31:42 -0300

    cep detail screen  initial commit

---
+++ lib/src/cep_detail/cep_detail.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CepDetailScreen extends StatefulWidget {
+  CepDetailScreen({Key key}) : super(key: key);
+
+  @override
+  _CepDetailScreenState createState() {
+    return _CepDetailScreenState();
+  }
+}
+
+class _CepDetailScreenState extends State<CepDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
